97f2bfd20b3ce986652270eaa0ddeca3a68c5bd0
Bretton <brettmay0212@gmail.com>
2025-03-28 17:44:51 -0700

    Implementing 'feed' scrolling, link previews, layout changes

--- baleen_mobile/lib/models/post.dart
+++ baleen_mobile/lib/models/post.dart
@@ -1,37 +1,42 @@
+import 'package:lemmy_api_client/v3.dart';
+
 class Post {
   final int id;
-  final String name;
-  final String body;
-  final String url;
-  final String creatorName;
-  final int score;
-  final DateTime published;
+  final int communityId;
+  final String title;
+  final String content;
+  final String? imageUrl;
   final String communityName;
+  final String authorName;
+  final DateTime published;
+  final int score;
   final int commentCount;
 
   Post({
     required this.id,
-    required this.name,
-    required this.body,
-    required this.url,
-    required this.creatorName,
-    required this.score,
-    required this.published,
+    required this.communityId,
+    required this.title,
+    required this.content,
+    this.imageUrl,
     required this.communityName,
+    required this.authorName,
+    required this.published,
+    required this.score,
     required this.commentCount,
   });
 
-  factory Post.fromJson(Map<String, dynamic> json) {
+  factory Post.fromLemmyPost(PostView postView) {
     return Post(
-      id: json['post']['id'],
-      name: json['post']['name'],
-      body: json['post']['body'] ?? '',
-      url: json['post']['url'] ?? '',
-      creatorName: json['creator']['name'],
-      score: json['counts']['score'],
-      published: DateTime.parse(json['post']['published']),
-      communityName: json['community']['name'],
-      commentCount: json['counts']['comments'],
+      id: postView.post.id,
+      communityId: postView.community.id,
+      title: postView.post.name,
+      content: postView.post.body ?? '',
+      imageUrl: postView.post.url,
+      communityName: postView.community.name,
+      authorName: postView.creator.name,
+      published: postView.post.published,
+      score: postView.counts.score,
+      commentCount: postView.counts.comments,
     );
   }
-}
+} 
